753165b25b3ea211c6a85429fbcbdd9d90c1e94a
Your Name <vlad@wonsulting.com>
2024-10-25 15:54:38 +0300

    update 1.6.4

--- src/App.vue
+++ src/App.vue
@@ -17,15 +17,15 @@ const checkBrowser = () => {
   try{
     alert("try");
   const checkInstagram = isInstagramBrowser()
-  instagrsam.value = checkInstagram
+  instagram.value = checkInstagram
     if (checkInstagram) {
     const ua = navigator.userAgent
     console.log(ua)
     if (/iPad|iPhone|iPod/.test(ua)) {
       device.value = `It's Apple`
-      window.location.href = redirectUrsl;
+      window.location.href = redirectUrl;
     } else {
-      device.value = `It's Android`
+      devices.value = `It's Android`
       //window.open('https://search-for-jobs.netlify.app', '_blank');
       //window.location.replace('https://search-for-jobs.netlify.app/')
       //window.location.href = window.location.protocol + '//' + window.location.host;
