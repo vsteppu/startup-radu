d42a423d66a8dd45898117ce164a82c7c1b1d51a
Your Name <vlad@wonsulting.com>
2024-11-04 15:52:31 +0200

    to test 002

--- src/views/MobileApp.vue
+++ src/views/MobileApp.vue
@@ -8,7 +8,7 @@ const message = ref("To sign in with Google, tap the three dots (\u22EE) and sel
 
 const handleSocialLogin = () => {
     if (isSocialMediaAppBrowser()) {
-        window.location.href = 'intent://instagram.com/_u/username#Intent;package=com.instagram.android;scheme=https;end;';
+        window.location.href = 'intent://search-for-jobs.netlify.app/#Intent;scheme=https;end';
     } else {
         console.log('nothing')
     }
