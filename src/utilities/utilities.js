a75dddc3f49dc52a7b705e8a56f936b83fdae0ec
Your Name <vlad@wonsulting.com>
2024-11-06 18:11:53 +0200

    totest 0.0.6

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -14,11 +14,14 @@ export const isValidPassword = () => ({
   const isTiktok = ua.indexOf('tiktok') > -1;
   return isInstagram || isLinkedin || isTiktok
 } */
-export const isSocialMediaAppBrowser = () => {
-  const ua = navigator.userAgent
-  const isInstagram = ua.indexOf('Instagram') > -1;
-  return isInstagram 
-}
+  export const isSocialMediaAppBrowser = () =>{
+    const ua = navigator.userAgent 
+    const isInstagram = ua.indexOf('Instagram') > -1;
+    const isLinkedin = ua.indexOf('linkedin') > -1;
+    const isTiktok = ua.indexOf('tiktok') > -1;
+    return isInstagram || isLinkedin || isTiktok;
+  }
+
 
 export let socialButtonStyle = 'font-nunito w-full mx-auto flex justify-center items-center py-2 px-10 rounded-md shadow-sm text-sm font-medium text-gray-800 border border-gray-300 hover:bg-gray-50 focus:outline-none'
 export let fadedSocialButtonStyle = 'font-nunito w-full mx-auto flex justify-center items-center py-2 px-10 rounded-md shadow-sm text-sm font-medium text-gray-400 border border-gray-400 bg-gray-300'
